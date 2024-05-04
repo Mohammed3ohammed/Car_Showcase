b3bd19a91a3126916496b5055d167a047ff64677
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2024-05-04 15:07:17 +0300

    design section  Explore the cars you might like

--- components/index.ts
+++ components/index.ts
@@ -1,13 +1,22 @@
+import { SearchManuFacturer } from './SearchManuFacturer';
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import CarCard from './CarCard';
+
+
 
 export {
     Hero,
     CustomButton,
     Navbar,
-    Footer
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManuFacturer,
+    CarCard
 
-    
 }
